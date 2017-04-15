eaa5c6ca94de88230ab12c793ba4881ba2b31b28
Iris Grace Endozo <irise@freelancer.com>
2017-04-16 01:23:44 +0800

    chore: initial configuration for traffic light template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TrafficLightComponent } from './traffic-light/traffic-light.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrafficLightComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/traffic-light/traffic-light.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-traffic-light',
+  templateUrl: './traffic-light.component.html',
+  styleUrls: ['./traffic-light.component.less']
+})
+export class TrafficLightComponent implements OnInit {
+  @Input() name: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
